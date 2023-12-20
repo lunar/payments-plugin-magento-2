09f9d9658315b2a20d3825ee0aa279898461a185
lucionescu <lucian.ionesq@gmail.com>
2023-12-20 15:26:09 +0200

    Fixed LogoUrl validation

--- Lunar_Payment/Model/Adminhtml/Source/LogoUrl.php
+++ Lunar_Payment/Model/Adminhtml/Source/LogoUrl.php
@@ -26,30 +26,53 @@ class LogoUrl extends Value
 			throw new LocalizedException(__('The image url must begin with https://.'));
 		}
 
-        if (! filter_var($logoUrl, FILTER_VALIDATE_URL)) {
-            /** Mark the new value as invalid */
+        if (!$this->fileExists($logoUrl)) {
             $this->_dataSaveAllowed = false;
-			throw new LocalizedException(__('The provided URL isn\'t valid'));
-		}
-
-        try {
-            $fileSpecs = getimagesize($logoUrl);
-        } catch (\Exception $e) {
             throw new LocalizedException(__('The image file doesn\'t seem to be valid'));
         }
 
-        $fileMimeType = explode('/', $fileSpecs['mime'] ?? '');
-        $fileExtension = end($fileMimeType);
+        // try {
+        //     $fileSpecs = getimagesize($logoUrl);
+        // } catch (\Exception $e) {
+        //     $this->_dataSaveAllowed = false;
+        //     throw new LocalizedException(__('The image file doesn\'t seem to be valid'));
+        // }
+        
+        // $fileMimeType = explode('/', $fileSpecs['mime'] ?? '');
+        // $fileExtension = end($fileMimeType);
 
-        // $fileDimensions = ($fileSpecs[0] ?? '') . 'x' . ($fileSpecs[1] ?? '');
-        // strcmp('250x250', $fileDimensions) !== 0      // disabled for the moment
+        // // $fileDimensions = ($fileSpecs[0] ?? '') . 'x' . ($fileSpecs[1] ?? '');
+        // // strcmp('250x250', $fileDimensions) !== 0      // disabled for the moment
 
-        if (! in_array($fileExtension, $allowedExtensions)) {
-            /** Mark the new value as invalid */
-            $this->_dataSaveAllowed = false;
-			throw new LocalizedException(__('The image file must have one of the following extensions: ' . implode(', ', $allowedExtensions)));
-		}
+        // if (! in_array($fileExtension, $allowedExtensions)) {
+        //     /** Mark the new value as invalid */
+        //     $this->_dataSaveAllowed = false;
+		// 	throw new LocalizedException(__('The image file must have one of the following extensions: ' . implode(', ', $allowedExtensions)));
+		// }
 
         return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    private function fileExists($url)
+    {
+        $valid = true;
+
+        $c = curl_init();
+        curl_setopt($c, CURLOPT_URL, $url);
+        curl_setopt($c, CURLOPT_HEADER, 1);
+        curl_setopt($c, CURLOPT_NOBODY, 1);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($c, CURLOPT_FRESH_CONNECT, 1);
+        
+        if(!curl_exec($c)){
+            $valid = false;
+        }
+
+        curl_close($c);
+
+        return $valid;
     }
 }
